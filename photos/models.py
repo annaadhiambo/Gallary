c11a6b1ee757fc0d37a45d79a2a69d9ca9a4ee43
annaadhiambo <annaadhiambo01@gmail.com>
2020-10-10 23:04:05 +0300

    added image class

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    title = models.CharField(max_length =30)
+    image = models.ImageField(max_length =60,upload_to='images/')
+    description = models.TextField(max_length =200)
+
+    
+    @classmethod
+    def search_by_title(cls,search_term):
+        news = cls.objects.filter(title__icontains=search_term)
+        return news
