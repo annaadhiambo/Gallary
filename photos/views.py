c5f4bed32f33971be798c56c71a49fe6e0949cbd
annaadhiambo <annaadhiambo01@gmail.com>
2020-10-12 07:02:02 +0300

    imported images

--- photos/views.py
+++ photos/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
-
+from django.http  import HttpResponse 
+from .models import Image
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    images = Image.objects.all()
+    context = {
+        'images':images
+    }
+    return render(request, 'home.html',context)
+    
+def search_results(request):
+    
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_photos = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        context = {
+            'images':searched_photos,
+            'message':message,
+        }
+
+        return render(request, 'photo/search.html',context)
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'photo/search.html',{"message":message})
+
